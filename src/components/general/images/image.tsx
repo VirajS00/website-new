c5fefecbdd474786e92d1f7c99ddbf5a72b5c1dc
Viraj Shukla <virajshukla00@gmail.com>
2024-05-01 23:57:32 +0530

    meta

--- src/components/general/images/image.tsx
+++ src/components/general/images/image.tsx
@@ -1,12 +1,13 @@
 import { component$ } from "@builder.io/qwik";
-import type { GetImageResult } from "astro";
+import type { ImageType } from "../../../types/image";
 
-export const Picture = component$<{ image: GetImageResult; class?: string }>(
+export const Picture = component$<{ image: ImageType; class?: string }>(
 	(props) => {
 		return (
 			<img
 				src={props.image.src}
 				{...props.image.attributes}
+				alt={props.image.data.caption}
 				class={{ [props.class ?? ""]: !!props.class }}
 			/>
 		);
