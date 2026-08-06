919bae3b36e9913511dba1fcd066ad2675af0190
Viraj Shukla <virajshukla00@gmail.com>
2026-08-06 22:45:05 +0530

    Initialized new project for new website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,5 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import qwikdev from "@qwikdev/astro";
-import icon from "astro-icon";
-import codeTitles from "remark-code-titles";
-
-import sitemap from "@astrojs/sitemap";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	site: "https://virajshukla.com",
-	integrations: [mdx(), icon(), qwikdev(), sitemap()],
-	markdown: {
-		shikiConfig: {
-			theme: "material-theme-darker",
-		},
-		remarkPlugins: [codeTitles],
-	},
-	image: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "**.ytimg.com",
-			},
-			{
-				protocol: "https",
-				hostname: "**.staticflickr.com",
-			},
-			{
-				protocol: "https",
-				hostname: "**.cloudinary.com",
-			},
-		],
-	},
-	compressHTML: true,
-});
+export default defineConfig({});
--- src/types/films-db-return-type.ts
+++
@@ -1,9 +0,0 @@
-export type FilmsTableReturnType = {
-	film_id: string;
-	my_role: string;
-	category: string;
-	film_short_description: string;
-	images: string[];
-	year: string;
-	sort_order: number;
-};
